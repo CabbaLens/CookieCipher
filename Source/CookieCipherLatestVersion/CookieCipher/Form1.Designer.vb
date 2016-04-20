3ed73cdb5df0bf7f31f43670881a49cb3e974204
Jaydyn <cyc0s63@gmail.com>
2016-04-20 15:41:14 -0400

    Working!
    its working

---
+++ Source/CookieCipherLatestVersion/CookieCipher/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(403, 505)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "the quick brown fox jumped over the lazy dog"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(0, 511)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(403, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Cipher"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 537)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(182, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "only ciphers a-z (no capitals, only a-z)"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(419, 511)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(403, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Decipher"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(419, 0)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(403, 505)
+        Me.TextBox2.TabIndex = 6
+        Me.TextBox2.Text = "{=20 ',-6l0 4*c+t0 uc}0 h,p#2_0 c;2*0 {=20 m8s~0 _cx"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.ClientSize = New System.Drawing.Size(819, 558)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(835, 596)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(50, 50)
+        Me.Name = "Form1"
+        Me.Text = "CookieCipher (Beta) (First working version)"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ Source/CookieCipherLatestVersion/CookieCipher/Form1.vb
@@ -0,0 +1,84 @@
+﻿Public Class Form1
+    Private Function cipher(ByVal encrypt As Boolean, ByVal s As String)
+        Dim r As String = Nothing
+        If (encrypt) Then
+            For Each c As Char In s
+                If (c = "a") Then r &= "8"
+                If (c = "b") Then r &= "4"
+                If (c = "c") Then r &= "6"
+                If (c = "d") Then r &= "_"
+                If (c = "e") Then r &= "2"
+                If (c = "f") Then r &= "u"
+                If (c = "g") Then r &= "x"
+                If (c = "h") Then r &= "="
+                If (c = "i") Then r &= "-"
+                If (c = "j") Then r &= "h"
+                If (c = "k") Then r &= "l"
+                If (c = "l") Then r &= "m"
+                If (c = "m") Then r &= "p"
+                If (c = "n") Then r &= "t"
+                If (c = "o") Then r &= "c"
+                If (c = "p") Then r &= "#"
+                If (c = "q") Then r &= "'"
+                If (c = "r") Then r &= "*"
+                If (c = "s") Then r &= "&"
+                If (c = "t") Then r &= "{"
+                If (c = "u") Then r &= ","
+                If (c = "v") Then r &= ";"
+                If (c = "w") Then r &= "+"
+                If (c = "x") Then r &= "}"
+                If (c = "y") Then r &= "~"
+                If (c = "z") Then r &= "s"
+                If (c = " ") Then r &= "0"
+                If (Not c = "a" And Not c = "b" And Not c = "c" And Not c = "d" And Not c = "e" And Not c = "f" And Not c = "g" And Not c = "h" And Not c = "i" And Not c = "j" And Not c = "k" And Not c = "l" And Not c = "m" And Not c = "n" And Not c = "o" And Not c = "p" And Not c = "q" And Not c = "r" And Not c = "s" And Not c = "t" And Not c = "u" And Not c = "v" And Not c = "w" And Not c = "x" And Not c = "y" And Not c = "z" And Not c = "c") Then r &= c
+            Next
+        Else
+            For Each c As Char In s
+                If (c = "8") Then r &= "a"
+                If (c = "4") Then r &= "b"
+                If (c = "6") Then r &= "c"
+                If (c = "_") Then r &= "d"
+                If (c = "2") Then r &= "e"
+                If (c = "u") Then r &= "f"
+                If (c = "x") Then r &= "g"
+                If (c = "=") Then r &= "h"
+                If (c = "-") Then r &= "i"
+                If (c = "h") Then r &= "j"
+                If (c = "l") Then r &= "k"
+                If (c = "m") Then r &= "l"
+                If (c = "p") Then r &= "m"
+                If (c = "t") Then r &= "n"
+                If (c = "c") Then r &= "o"
+                If (c = "#") Then r &= "p"
+                If (c = "'") Then r &= "q"
+                If (c = "*") Then r &= "r"
+                If (c = "&") Then r &= "s"
+                If (c = "{") Then r &= "t"
+                If (c = ",") Then r &= "u"
+                If (c = ";") Then r &= "v"
+                If (c = "+") Then r &= "w"
+                If (c = "}") Then r &= "x"
+                If (c = "~") Then r &= "y"
+                If (c = "s") Then r &= "z"
+                If (c = "0") Then r &= " "
+                If (Not c = "8" And Not c = "4" And Not c = "6" And Not c = "_" And Not c = "2" And Not c = "u" And Not c = "x" And Not c = "=" And Not c = "-" And Not c = "h" And Not c = "l" And Not c = "m" And Not c = "p" And Not c = "t" And Not c = "c" And Not c = "#" And Not c = "'" And Not c = "*" And Not c = "&" And Not c = "{" And Not c = "," And Not c = ";" And Not c = "}" And Not c = "~" And Not c = "0") Then r &= c
+            Next
+        End If
+        Return r
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = cipher(True, TextBox1.Text)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+        TextBox2.Text = cipher(False, TextBox2.Text)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox2.Text = cipher(False, TextBox2.Text)
+    End Sub
+End Class
