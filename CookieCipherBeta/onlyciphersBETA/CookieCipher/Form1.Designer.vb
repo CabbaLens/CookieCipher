a16ecca6881e6830e8fa82f42dad1479671f481d
Jaydyn <cyc0s63@gmail.com>
2016-04-20 13:56:26 -0400

    Only ciphers
    i made the cipher now on to the decipher

---
+++ CookieCipherBeta/onlyciphersBETA/CookieCipher/Form1.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(403, 505)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "Text to cipher"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(0, 511)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(403, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Cipher"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(-3, 537)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "only ciphers"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(81, 539)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(187, 540)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "decipher"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(411, 558)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(835, 596)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(50, 50)
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.Text = "CookieCipher (Beta)"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button2 As Button
+End Class
---
+++ CookieCipherBeta/onlyciphersBETA/CookieCipher/Form1.vb
@@ -0,0 +1,55 @@
+﻿Public Class Form1
+    Private Function cipher(ByVal encrypt As Boolean, ByVal s As String)
+        Dim r As String = Nothing
+        If (encrypt) Then
+            For Each c As Char In s
+                If (c = "a") Then r &= "nutella"
+                If (c = "b") Then r &= "mrnoodles"
+                If (c = "c") Then r &= "coffee"
+                If (c = "d") Then r &= "strawberry"
+                If (c = "e") Then r &= "ultra"
+                If (c = "f") Then r &= "atomic"
+                If (c = "g") Then r &= "fork"
+                If (c = "b") Then r &= "pen"
+                If (c = "i") Then r &= "-"
+                If (c = "j") Then r &= "hostipal"
+                If (c = "k") Then r &= "lid"
+                If (c = "l") Then r &= "mat"
+                If (c = "m") Then r &= "police"
+                If (c = "n") Then r &= "spork"
+                If (c = "o") Then r &= "cherry"
+                If (c = "p") Then r &= "dog"
+                If (c = "q") Then r &= "chips"
+                If (c = "r") Then r &= "food"
+                If (c = "s") Then r &= "trust"
+                If (c = "t") Then r &= "yes"
+                If (c = "u") Then r &= "head"
+                If (c = "v") Then r &= "call"
+                If (c = "w") Then r &= "no"
+                If (c = "x") Then r &= "life"
+                If (c = "y") Then r &= "bored"
+                If (c = "z") Then r &= "whatever"
+                If (c = " ") Then r &= "_"
+                If (Not c = "a" And Not c = "b" And Not c = "c" And Not c = "d" And Not c = "e" And Not c = "f" And Not c = "g" And Not c = "h" And Not c = "i" And Not c = "j" And Not c = "k" And Not c = "l" And Not c = "m" And Not c = "n" And Not c = "o" And Not c = "p" And Not c = "q" And Not c = "r" And Not c = "s" And Not c = "t" And Not c = "u" And Not c = "v" And Not c = "w" And Not c = "x" And Not c = "y" And Not c = "z" And Not c = "c") Then r &= c
+            Next
+        Else
+            For Each c As Char In s
+                If (c = "h") Then r &= "a"
+                If (c = "l") Then r &= "b"
+                If (Not c = "h" And Not c = "l") Then r &= c
+            Next
+        End If
+        Return r
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = cipher(True, TextBox1.Text)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+        TextBox2.Text = cipher(False, TextBox2.Text)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
